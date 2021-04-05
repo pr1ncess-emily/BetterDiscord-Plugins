e908d63bb4d7f8759d46c6c1b8a6678d15a5cb6b
b3nb0t5000 <b3nb0t5000@gmail.com>
2021-04-04 21:46:13 -0600

    escaping link

--- StatusScheduler/status-scheduler.plugin.js
+++ StatusScheduler/status-scheduler.plugin.js
@@ -10,42 +10,41 @@
 
 module.exports = class StatusScheduler {
 
-        // Meta (i know i already added a meta but i'm gonna add another one cause i'm just like that)
-        getName() {
-            return "StatusScheduler";
-        }
-        getDescription() {
-            return "BetterDiscord plugin to schedule / time Discord statuses (online, idle, DND, etc.)";
-        }
-        getVersion() {
-            return "0.0.1";
-        }
-        getAuthor() {
-            return "b3nb0t5000";
-        }
+    // Meta (i know i already added a meta but i'm gonna add another one cause i'm just like that)
+    getName() {
+        return "StatusScheduler";
+    }
+    getDescription() {
+        return "BetterDiscord plugin to schedule / time Discord statuses (online, idle, DND, etc.)";
+    }
+    getVersion() {
+        return "0.0.1";
+    }
+    getAuthor() {
+        return "b3nb0t5000";
+    }
 
-        // Settings panel
-        getSettingsPanel() {
-            return "<-- This is my BetterDiscord Status Scheduler plugin that I have no idea how I'm gonna code! !-->";
+    // Settings panel
+    getSettingsPanel() {
+        return "<-- This is my BetterDiscord Status Scheduler plugin that I have no idea how I'm gonna code! !-->";
+    }
+
+    // plugin start
+    start() {
+        //throwing error if zeres library isn't present
+        if (!global.ZeresPluginLibrary) {
+            return window.BdApi.alert("Library missing", "A library required for this program to run is missing. You can install it from <a href=\" //raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js\" target=\"_blank\">here</a>.");
         }
+    }
 
-        // plugin start
-        start() {
-            //throwing error if zeres library isn't present
-            if (!global.ZeresPluginLibrary) {
-                return window.BdApi.alert("Library missing", "A library required for this program to run is missing. You can install it from <a href="
-                    https: //raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">here</a>.");
-                }
-            }
+    var separator = ZLibrary.DOMTools.createElement('<div>');
+    ZLibrary.DOMTools.addClass(separator, "separator-2 I32lJ da-separator");
+    var menuButton = ZLibrary.DOMTools.createElement('<p>Set Status timer...</p>');
+    var statusPicker = ZLibrary.DOMTools.Q('#status-picker');
+    ZLibrary.DOMTools.prependTo(separator, statusPicker);
+    ZLibrary.DOMTools.prepentTo(menuButton, statusPicker);
 
-            var separator = ZLibrary.DOMTools.createElement('<div>');
-            ZLibrary.DOMTools.addClass(separator, "separator-2 I32lJ da-separator");
-            var menuButton = ZLibrary.DOMTools.createElement('<p>Set Status timer...</p>');
-            var statusPicker = ZLibrary.DOMTools.Q('#status-picker');
-            ZLibrary.DOMTools.prependTo(separator, statusPicker);
-            ZLibrary.DOMTools.prepentTo(menuButton, statusPicker);
+    // plugin stop
+    stop() {} // Required function. Called when the plugin is deactivated
 
-            // plugin stop
-            stop() {} // Required function. Called when the plugin is deactivated
-
-        }
+}
